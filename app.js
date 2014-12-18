eaadf01ebd45cdea31d1fdfc7a36f1ef5ebc0045
Paul Lessing <paul@paullessing.com>
2014-12-18 22:24:15 +0000

    Change code to not need listeners in frontend.js, and switch relay

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 var userManager = require('./lib/user-manager');
 
+var frontend = require('./lib/frontend');
+frontend.setUsers(userManager.getUsersHome());
+
+var relay = require('./lib/relay');
+relay.off();
+var offTimer = null;
+var status = false;
+
 userManager.on('userConnected', function(user) {
-	console.log("User connected", user);
+//	console.log("User connected", user);
+	frontend.userConnected(user);
+	relay.on();
+	if (offTimer) {
+		clearTimeout(offTimer);
+	}
+	offTimer = setTimeout(function() {
+		relay.off();
+		offTimer = null;
+	}, 15000); // TODO make configurable
+	
 });
 userManager.on('userDisconnected', function(user) {
-	console.log("User disconnected", user);
+//	console.log("User disconnected", user);
+	frontend.userDisconnected(user);
 });
-
-var frontend = require('./lib/frontend')(userManager);
