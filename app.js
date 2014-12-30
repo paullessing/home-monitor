ec4cd8e213ece20d871709bdf16f1ae7971c0e19
paul.lessing <paul@paullessing.com>
2014-12-30 15:42:23 +0100

    Introduced Mock MacRouter for testing

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
+var config = require('./config/config');
+
 var userManager = require('./lib/user-manager');
 
 var frontend = require('./lib/frontend');
 frontend.setUsers(userManager.getUsersHome());
 
 var relay = require('./lib/relay');
-relay.off();
-var offTimer = null;
 var status = false;
 
 userManager.on('userConnected', function(user) {
 //	console.log("User connected", user);
 	frontend.userConnected(user);
-	relay.on();
-	if (offTimer) {
-		clearTimeout(offTimer);
-	}
-	offTimer = setTimeout(function() {
-		relay.off();
-		offTimer = null;
-	}, 15000); // TODO make configurable
-	
+	relay.onFor(config.relay.onDurationSeconds);
 });
+
 userManager.on('userDisconnected', function(user) {
 //	console.log("User disconnected", user);
 	frontend.userDisconnected(user);
---
+++ config/config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.relay = {};
+config.relay.pin = 7;
+// Time the relay stays on after a user connects.
+config.relay.onDurationSeconds = 15;
+
+module.exports = config;
